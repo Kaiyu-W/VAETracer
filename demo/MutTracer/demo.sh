0392c754c05ef992e2cd3bf8d3bd8f0cdcd8fbde
Kaiyu-W <79740043+Kaiyu-W@users.noreply.github.com>
2026-02-12 22:09:27 +0800

    Add files via upload

--- demo/MutTracer/demo.sh
+++ demo/MutTracer/demo.sh
@@ -1,24 +1,22 @@
-
-
 #!/bin/bash
-
-OUTPUT_DIR="./VAETracer/demo/MutTracer/output_test"
-mkdir -p $OUTPUT_DIR
+VAETRACER_DIR=$(readlink -m ../..)
+OUTPUT_DIR="${VAETRACER_DIR}/demo/MutTracer/output_test"
+mkdir -p $OUTPUT_DIR && cd $VAETRACER_DIR
 
 echo "Running MutTracer test..."
 
-python -m MutTracer.main \
-  --scmut_model_path ./VAETracer/demo/MutTracer/input_test/zmt_model_test.pkl \
-  --zmt_path ./VAETracer/demo/MutTracer/input_test/z_mt_test.pt \
-  --zxt_path ./VAETracer/demo/MutTracer/input_test/z_xt_test.pt \
+PYTHONPATH=${VAETRACER_DIR}/.. python -m MutTracer.main \
+  --scmut_model_path ${VAETRACER_DIR}/demo/MutTracer/input_test/zmt_model_test.pkl \
+  --zmt_path ${VAETRACER_DIR}/demo/MutTracer/input_test/z_mt_test.pt \
+  --zxt_path ${VAETRACER_DIR}/demo/MutTracer/input_test/z_xt_test.pt \
   --input_times 1 2 \
   --predict_times 0 \
   --epochs 20 \
-  --adata_path ./VAETracer/demo/MutTracer/input_test/exp.h5ad \
-  --scvi_model_path ./VAETracer/demo/MutTracer/input_test/exp_scvi_model.pkl \
+  --adata_path ${VAETRACER_DIR}/demo/MutTracer/input_test/exp.h5ad \
+  --scvi_model_path ${VAETRACER_DIR}/demo/MutTracer/input_test/exp_scvi_model.pkl \
   --save_dir $OUTPUT_DIR \
   --real_times_keep 1 2  --pred_times_keep 1 \
-  > /dev/null 2>&1
+  > ${OUTPUT_DIR}/demo.log 2>&1
 
 
 echo "Checking output..."
